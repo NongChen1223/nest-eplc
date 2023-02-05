181b3fbb6e84d08dca998d547f630d2c9c8bed21
NongChen <2584533077@qq.com>
2023-02-06 00:21:32 +0800

    新增user typeorm实体类

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'root',
+      password: 'xww19981223',
+      host: 'localhost',
+      port: 3306,
+      database: 'nest',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], //实体文件
+      synchronize: true, //synchronize字段代表是否自动将实体类同步到数据库
+      retryDelay: 500, //重试连接数据库间隔
+      retryAttempts: 10, //重试连接数据库的次数
+      autoLoadEntities: true, //如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity() // @Entity()装饰器自动从所有类生成一个SQL表，以及他们包含的元数据
+// @Entity('users') // sql表名为users
+export class User {
+  // 主键装饰器，也会进行自增
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+}
