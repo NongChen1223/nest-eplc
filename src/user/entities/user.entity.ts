3349d01adf550acbc79848b7d78cffe8ad62e857
NongChen <2584533077@qq.com>
2023-02-08 23:42:06 +0800

    user typeorm 创建实体类 新建SQL表

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Generated,
+} from 'typeorm';
 @Entity() // @Entity()装饰器自动从所有类生成一个SQL表，以及他们包含的元数据
 // @Entity('users') // sql表名为users
 export class User {
@@ -6,9 +12,18 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ type: 'varchar', length: 255 })
   name: string;
 
-  @Column()
+  @Column({ select: true, comment: '登录密码' })
   password: string;
+
+  @Generated('uuid')
+  uuid: string;
+
+  @Column()
+  Eamil: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createTime: Date;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { Repository, Like } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly user: Repository<User>,
+  ) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
